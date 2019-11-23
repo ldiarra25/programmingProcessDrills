f6f53ce55edefbacd76d078625c896411aec5b0f
ldiarra25 <55326296+ldiarra25@users.noreply.github.com>
2019-11-23 11:14:21 -0600

    Initial commit

---
+++ textBasedGame.py
@@ -0,0 +1,15 @@
+from pip._vendor.requests.__version__ import __cake__
+Objective
+The main objective is to create a text bassed adventure game
+1) Print that it i your grandmother (name) birthday adn she likes (flavor) cake.
+2) Make a function that takes inout from the user about what flavor cake to buy
+3) Make a function that takes input from the user about what words to put on the cake 
+4) Print what flavor of cake the user put in and the words they chose to put of the cake 
+5) Make a function that checks if the users cake is the same as what grandmother askedfor 
+    a)Grandmother responds apropriatly to wethwe the player got the cake flavor right or not 
+    
+Pseudocode
+ranName = ["Mildred", "Ruth", "Nancy", "Connie", "Edith", "Agnes"]
+Name = ranname [randint(0,5)]
+randFlavor = ["chocolate", "vanilla", "strawberry", "red velvet", "coconut"]
+flavor = randFlavor [randint(0,5)]
